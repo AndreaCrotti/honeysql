b930d6e9083d524897f48383b5cd2e3b41777ddc
Michael Blume <blume.mike@gmail.com>
2017-11-06 22:00:03 -0800

    Revert "Add ClojureScript self-host support"

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :url "https://github.com/jkk/honeysql"
   :scm {:name "git"
         :url "https://github.com/jkk/honeysql"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.cgrand/macrovich "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :aliases {"test-readme" ["with-profile" "midje" "midje"]}
   :cljsbuild {:builds {:release {:source-paths ["src"]
                                  :compiler {:output-to "dist/honeysql.js"
@@ -22,8 +21,6 @@
                                          :parallel-build true
                                          :target :nodejs}}}}
   :doo {:build "test"}
-  :tach {:test-runner-ns 'honeysql.self-host-runner
-         :source-paths ["src" "test"]}
   :profiles {:midje {:dependencies [[midje "1.9.0-alpha5"]]
                      :plugins      [[lein-midje "3.2.1"]
                                     [midje-readme "1.0.9"]]
@@ -36,5 +33,4 @@
                                   [cljsbuild "1.1.6"]]
                    :plugins [[lein-cljsbuild "1.1.6"]
                              [jonase/eastwood "0.2.4"]
-                             [lein-doo "0.1.6"]
-                             [lein-tach "0.4.0"]]}})
+                             [lein-doo "0.1.6"]]}})
